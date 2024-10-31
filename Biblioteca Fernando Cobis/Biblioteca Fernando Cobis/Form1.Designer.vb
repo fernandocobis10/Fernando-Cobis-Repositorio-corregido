23f4127b9490f5f97383a35e9e077adb03a0bcc3
fernandocobis10 <fernandocobis50@gmail.com>
2024-10-31 19:20:02 -0400

    Add files via upload

---
+++ Biblioteca Fernando Cobis/Biblioteca Fernando Cobis/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(143, 221)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(520, 221)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(155, 192)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(99, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Prestar Libro"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(532, 192)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(99, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "Devolver Libro"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 24F)
+        Label1.Location = New Point(271, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(264, 45)
+        Label1.TabIndex = 4
+        Label1.Text = "Biblioteca Fercho"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F)
+        Label2.Location = New Point(271, 367)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(211, 21)
+        Label2.TabIndex = 5
+        Label2.Text = "Fernando Cobis / 30.816.831"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(-68, -14)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(937, 518)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 6
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Biblioteca Fernando Cobis/Biblioteca Fernando Cobis/Form1.vb
@@ -0,0 +1,46 @@
+﻿
+Public Class Form1
+    Private books As Dictionary(Of String, Boolean) = New Dictionary(Of String, Boolean) From {
+        {"Harry potter", True},
+        {"El principito", True},
+    }
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.AddRange(books.Keys.ToArray())
+        ComboBox2.Items.AddRange(books.Keys.ToArray())
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedItem Is Nothing Then
+            MessageBox.Show("Por favor, selecciona un libro para prestar.")
+            Return
+        End If
+
+        Dim selectedBook As String = ComboBox1.SelectedItem.ToString()
+        If books(selectedBook) Then
+            books(selectedBook) = False
+            MessageBox.Show(selectedBook & " prestado exitosamente.")
+        Else
+            MessageBox.Show(selectedBook & " ya está prestado.")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ComboBox2.SelectedItem Is Nothing Then
+            MessageBox.Show("Por favor, selecciona un libro para devolver.")
+            Return
+        End If
+
+        Dim selectedBook As String = ComboBox2.SelectedItem.ToString()
+        If Not books(selectedBook) Then
+            books(selectedBook) = True
+            MessageBox.Show(selectedBook & " devuelto exitosamente.")
+        Else
+            MessageBox.Show(selectedBook & " ya está en la biblioteca.")
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
