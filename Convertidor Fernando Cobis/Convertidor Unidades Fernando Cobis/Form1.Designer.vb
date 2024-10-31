bb3493d32909eb0f0013f379e0751856aec6bb0c
fernandocobis10 <fernandocobis50@gmail.com>
2024-10-31 19:21:57 -0400

    Add files via upload

---
+++ Convertidor Fernando Cobis/Convertidor Unidades Fernando Cobis/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        ComboBox3 = New ComboBox()
+        Tbx_cantidad = New TextBox()
+        Tbx_resultado = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(254, 210)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(254, 274)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 1
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Location = New Point(387, 107)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(121, 23)
+        ComboBox3.TabIndex = 2
+        ' 
+        ' Tbx_cantidad
+        ' 
+        Tbx_cantidad.Location = New Point(584, 210)
+        Tbx_cantidad.Name = "Tbx_cantidad"
+        Tbx_cantidad.Size = New Size(100, 23)
+        Tbx_cantidad.TabIndex = 3
+        ' 
+        ' Tbx_resultado
+        ' 
+        Tbx_resultado.Location = New Point(584, 274)
+        Tbx_resultado.Name = "Tbx_resultado"
+        Tbx_resultado.Size = New Size(100, 23)
+        Tbx_resultado.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F)
+        Label1.Location = New Point(501, 210)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(72, 21)
+        Label1.TabIndex = 5
+        Label1.Text = "Cantidad"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F)
+        Label2.Location = New Point(234, 107)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(138, 21)
+        Label2.TabIndex = 6
+        Label2.Text = "Unidad a convertir"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F)
+        Label3.Location = New Point(499, 274)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(79, 21)
+        Label3.TabIndex = 7
+        Label3.Text = "Resultado"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 12F)
+        Label4.Location = New Point(110, 212)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(138, 21)
+        Label4.TabIndex = 8
+        Label4.Text = "Unidad a convertir"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 12F)
+        Label5.Location = New Point(110, 274)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(138, 21)
+        Label5.TabIndex = 9
+        Label5.Text = "Unidad a convertir"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 23F)
+        Label6.Location = New Point(222, 40)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(367, 42)
+        Label6.TabIndex = 10
+        Label6.Text = "Convertidor de Unidades "
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(-22, -11)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(850, 477)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 11
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Tbx_resultado)
+        Controls.Add(Tbx_cantidad)
+        Controls.Add(ComboBox3)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Tbx_cantidad As TextBox
+    Friend WithEvents Tbx_resultado As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Convertidor Fernando Cobis/Convertidor Unidades Fernando Cobis/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Windows.Forms
+
+Public Class Form1
+    ' Constantes de Masa
+    Private Const KILO As Double = 1000
+    Private Const HECTO As Double = 100
+    Private Const DECA As Double = 10
+    Private Const GRAMO As Double = 1
+    Private Const DECI As Double = 0.1
+    Private Const CENTI As Double = 0.01
+    Private Const MILI As Double = 0.001
+
+    ' Constantes de Distancia
+    Private Const KILOMETRO As Double = 1000
+    Private Const METRO As Double = 1
+    Private Const DECIMETRO As Double = 0.1
+    Private Const CENTIMETRO As Double = 0.01
+    Private Const MILIMETRO As Double = 0.001
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim masas() As String = {"KILO", "HECTO", "DECA", "GRAMO", "DECI", "CENTI", "MILI"}
+        Dim distancias() As String = {"KILOMETRO", "METRO", "DECIMETRO", "CENTIMETRO", "MILIMETRO"}
+        ComboBox3.Items.AddRange(New String() {"Masa", "Distancia"})
+        ComboBox3.SelectedIndex = 0
+        ComboBox1.Items.AddRange(masas)
+        ComboBox2.Items.AddRange(masas)
+        ComboBox1.SelectedIndex = 3
+        ComboBox2.SelectedIndex = 3
+    End Sub
+
+    Private Sub Tbx_cantidad_TextChanged(sender As Object, e As EventArgs) Handles Tbx_cantidad.TextChanged
+        Convertir()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Convertir()
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Convertir()
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        If ComboBox3.SelectedItem.ToString() = "Masa" Then
+            ComboBox1.Items.Clear()
+            ComboBox2.Items.Clear()
+            Dim masas() As String = {"KILO", "HECTO", "DECA", "GRAMO", "DECI", "CENTI", "MILI"}
+            ComboBox1.Items.AddRange(masas)
+            ComboBox2.Items.AddRange(masas)
+            ComboBox1.SelectedIndex = 3
+            ComboBox2.SelectedIndex = 3
+        ElseIf ComboBox3.SelectedItem.ToString() = "Distancia" Then
+            ComboBox1.Items.Clear()
+            ComboBox2.Items.Clear()
+            Dim distancias() As String = {"KILOMETRO", "METRO", "DECIMETRO", "CENTIMETRO", "MILIMETRO"}
+            ComboBox1.Items.AddRange(distancias)
+            ComboBox2.Items.AddRange(distancias)
+            ComboBox1.SelectedIndex = 1
+            ComboBox2.SelectedIndex = 1
+        End If
+        Convertir()
+    End Sub
+
+    Private Sub Convertir()
+        Dim cantidad As Double
+        If Not Double.TryParse(Tbx_cantidad.Text, cantidad) Then
+            Tbx_resultado.Text = "0"
+            Return
+        End If
+        Dim unidadOrigen As String = ComboBox1.SelectedItem.ToString()
+        Dim unidadDestino As String = ComboBox2.SelectedItem.ToString()
+        Dim factorOrigen As Double = ObtenerFactor(unidadOrigen, ComboBox3.SelectedItem.ToString())
+        Dim factorDestino As Double = ObtenerFactor(unidadDestino, ComboBox3.SelectedItem.ToString())
+        Dim cantidadEnUnidad As Double = cantidad * factorOrigen
+        Dim resultado As Double = cantidadEnUnidad / factorDestino
+        Tbx_resultado.Text = resultado.ToString("F3")
+    End Sub
+
+    Private Function ObtenerFactor(unidad As String, tipo As String) As Double
+        Select Case tipo.ToUpper()
+            Case "MASA"
+                Select Case unidad.ToUpper()
+                    Case "KILO"
+                        Return KILO
+                    Case "HECTO"
+                        Return HECTO
+                    Case "DECA"
+                        Return DECA
+                    Case "GRAMO"
+                        Return GRAMO
+                    Case "DECI"
+                        Return DECI
+                    Case "CENTI"
+                        Return CENTI
+                    Case "MILI"
+                        Return MILI
+                    Case Else
+                        Return 1
+                End Select
+            Case "DISTANCIA"
+                Select Case unidad.ToUpper()
+                    Case "KILOMETRO"
+                        Return KILOMETRO
+                    Case "METRO"
+                        Return METRO
+                    Case "DECIMETRO"
+                        Return DECIMETRO
+                    Case "CENTIMETRO"
+                        Return CENTIMETRO
+                    Case "MILIMETRO"
+                        Return MILIMETRO
+                    Case Else
+                        Return 1
+                End Select
+            Case Else
+                Return 1
+        End Select
+    End Function
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        ' Añadir código aquí si es necesario
+    End Sub
+End Class
